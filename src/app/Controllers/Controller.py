213b27d61544e4390aacffb98ee718d601724fd8
Jordan Koeller <jkoeller@trinity.edu>
2017-09-07 16:05:55 +0000

    Documentation for Controller base class. Also now type-checks delegates in the method addChild

--- src/app/Controllers/Controller.py
+++ src/app/Controllers/Controller.py
@@ -17,12 +17,34 @@ class Controller(object):
     calculate(*args,**kwargs)
         Method to be called to perform the Controller's role. Must return something,
         even if it is None.
+        
+    run(*args)
+        Method that recurses through all delegates, calling their respective calculate methods. 
+        If self is deactivated before, by calling self.deactivate(), this method does nothing.
+        
+    addChild(delegate)
+        adds child to this instance's children. This controller will now call delegate.calculate() when 
+        looping through its children.
+        
+    deactivate()
+        Deactivate this controller. After calling this method, calls to self.run() will do nothing, and will not 
+        recurse down to its children. If the controller is already deactivated, this does nothing.
+    
+    activate()
+        Reactivates this controller, allowing calls to self.run() to execute. If the controller is already active,
+        this does nothing.
+        
+    ____________________
+    
+    Attributes:
+    
+    children: list[Controller] 
     '''
     _children = []
 
     def __init__(self):
         '''
-        Constructor
+        Constructor. No Arguments.
         '''
         self._children = []
         self._active = True
@@ -36,6 +58,11 @@ class Controller(object):
         return args
 
     def run(self,*args):
+    '''
+    Method that recurses through all delegates, calling their respective calculate methods. 
+    
+    If self is deactivated before, by calling self.deactivate(), this method does nothing.
+    '''
         if self._active:
             args = self.calculate(*args)
             for child in self._children:
@@ -43,11 +70,25 @@ class Controller(object):
 
 
     def addChild(self, child):
-        self._children.append(child)
-
+        if isinstance(child,Controller):
+            self._children.append(child)
+        else:
+            raise ValueError("child mustbe of type Controller")
+            
+            
     def deactivate(self):
-        print("Deactivated")
+        """
+        Deactivate this controller. After calling this method, calls to self.run() will do nothing, and will not 
+        recurse down to its children. If the controller is already deactivated, this does nothing.
+        """
         self._active = False
+        
+    def activate(self):
+        """
+        Reactivates this controller, allowing calls to self.run() to execute. If the controller is already active,
+        this does nothing.
+        """
+        self._active = True
 
     @property
     def children(self):
