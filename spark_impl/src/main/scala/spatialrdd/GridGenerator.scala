37088267c039ac602ee02b17f27f4ce6d70e4005
JordanKoeller <jkoeller12@gmail.com>
2018-05-29 13:13:47 -0500

    fdsa

--- spark_impl/src/main/scala/spatialrdd/GridGenerator.scala
+++ spark_impl/src/main/scala/spatialrdd/GridGenerator.scala
@@ -5,7 +5,7 @@ case class QueryInfo(px: Int, py: Int, x: Double, y: Double)
 class GridGenerator(x0: Double, y0: Double, x1: Double, y1: Double,val xDim: Int,val yDim: Int) extends Iterator[QueryInfo] with Serializable {
   private val xStep = (x1 - x0) / (xDim.toDouble)
   private val yStep = (y1 - y0) / (yDim.toDouble)
-  private val generator = (x: Double, y: Double) => (x0 + xStep * x, y1 - yStep * y)
+  private val generator = (x: Double, y: Double) => (x0 + xStep * x, y1 + yStep * y)
 
   private var currX: Int = 0
   private var currY: Int = 0
