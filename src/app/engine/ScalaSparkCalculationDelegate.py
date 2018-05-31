48d66614e09f37144078b8bf49e6deb810b9ca18
JordanKoeller <jkoeller12@gmail.com>
2018-05-31 09:42:34 -0500

    adding jar

--- src/app/engine/ScalaSparkCalculationDelegate.py
+++ src/app/engine/ScalaSparkCalculationDelegate.py
@@ -149,13 +149,14 @@ def _get_or_create_context(p):
         from pyspark.context import SparkContext
         settings = GlobalPreferences['spark_configuration']
         jarpath = GlobalPreferences['path']+"/spark_impl//target/scala-2.11/lensing_simulator_spark_kernel-assembly-0.1.0-SNAPSHOT.jar"
+        os.environ['SPARK_CLASSPATH'] = jarpath
         SparkContext.setSystemProperty("spark.executor.memory",settings['executor-memory'])
         SparkContext.setSystemProperty("spark.driver.memory",settings['driver-memory'])
         conf = SparkConf().setAppName(p.jsonString)
         conf = conf.setMaster(settings['master'])
         conf = conf.set('spark.driver.maxResultSize',settings['driver-memory'])
         _sc = SparkContext(conf=conf)
-        _sc._jsc.sc().addJar(jarpath)
+        _sc._jsc.addJar(jarpath)
         _sc.setLogLevel("WARN")
     return _sc
         
