aa469c8a8b9b35187eee116c8b8eafdc188fd3ce
JordanKoeller <jkoeller12@gmail.com>
2018-06-02 16:03:57 -0500

    fdsa

---
+++ scripts/config_pyspark_laptop.sh
@@ -0,0 +1,14 @@
+
+cd ../src
+export PYSPARK_DRIVER_PYTHON=ipython
+
+#Pull out the configuration variables
+MASTER=$(python -c 'from app.preferences import GlobalPreferences; print(GlobalPreferences["spark_configuration"]["master"])')
+DRIVER_MEMORY=$(python -c 'from app.preferences import GlobalPreferences; print(GlobalPreferences["spark_configuration"]["driver-memory"])')
+EXECUTOR_MEMORY=$(python -c 'from app.preferences import GlobalPreferences; print(GlobalPreferences["spark_configuration"]["executor-memory"])')
+EXTRA_ARGS=$(python -c 'from app.preferences import GlobalPreferences; print(GlobalPreferences["spark_configuration"]["command-line-args"])')
+JAR_LOC='../spark_impl/target/scala-2.11/lensing_simulator_spark_kernel-assembly-0.1.0-SNAPSHOT.jar'
+
+
+#The command itself
+/home/raqmu/Desktop/spark-2.2.0-bin-hadoop2.7/bin/pyspark --master $MASTER --executor-memory $EXECUTOR_MEMORY --driver-memory $DRIVER_MEMORY --jars $JAR_LOC
