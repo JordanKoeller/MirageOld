279743a2f27455c6b4a6f71e024616925b11b452
JordanKoeller <jkoeller12@gmail.com>
2018-06-01 19:47:58 -0500

    Script for pyspark integration

---
+++ scripts/config_pyspark.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#cd ../src
+export PYSPARK_DRIVER_PYTHON=ipython
+#Pull out the configuration variables
+MASTER=$(python -c 'from app.preferences import GlobalPreferences; print(GlobalPreferences["spark_configuration"]["master"])')
+DRIVER_MEMORY=$(python -c 'from app.preferences import GlobalPreferences; print(GlobalPreferences["spark_configuration"]["driver-memory"])')
+EXECUTOR_MEMORY=$(python -c 'from app.preferences import GlobalPreferences; print(GlobalPreferences["spark_configuration"]["executor-memory"])')
+EXTRA_ARGS=$(python -c 'from app.preferences import GlobalPreferences; print(GlobalPreferences["spark_configuration"]["command-line-args"])')
+JAR_LOC='../spark_impl/target/scala-2.11/lensing_simulator_spark_kernel-assembly-0.1.0-SNAPSHOT.jar'
+
+
+ARGS='--master $Master --executor-memory $EXECUTOR_MEMORY --driver-memory $DRIVER_MEMORY --jars $JAR_LOC --conf spark.driver.maxResultSize=$DRIVER_MEMORY'
+/home/raqmu/miniconda3/bin/pyspark #$ARGS
